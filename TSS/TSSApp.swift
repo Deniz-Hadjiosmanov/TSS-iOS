e50e2fa5591b76630b3b916849ccdc598cfe4433
Martin Grigorov <485519@student.fontys.nl>
2023-03-05 20:50:35 +0100

    Initial Commit

---
+++ TSS/TSSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TSSApp.swift
+//  TSS
+//
+//  Created by Deniz Hadjiosmanov on 05/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct TSSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
