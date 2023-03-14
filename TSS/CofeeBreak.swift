5cf03aa2235c627a04f1b10a38f73055e5e15a9f
Martin Grigorov <485519@student.fontys.nl>
2023-03-14 15:55:13 +0100

    Added background blur when the popup is displayed.

--- TSS/CofeeBreak.swift
+++ TSS/CofeeBreak.swift
@@ -60,72 +60,97 @@ struct CoffeeBreak: View {
                 }
                 
                 Button(action: {
-                    self.isButtonPressed = true
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-                        self.isPopupVisible = true
-                    }
-                }) {
-                    Text("Start")
-                        .font(.system(size: 20, weight: .semibold))
-                        .foregroundColor(.white)
-                        .padding(.all, 10)
-                        .padding(.horizontal, 50.0)
-                        .background(Color.black)
-                        .cornerRadius(10)
-                        .padding(.top, 600.0)
-                }
-                
-                if isPopupVisible {
-                    Rectangle()
-                        .fill(Color.white)
-                        .frame(width: 300, height: 400)
-                        .cornerRadius(20)
-                        .shadow(radius: 10)
-                        .overlay(
-                            VStack {
-                                Text("Your coffee partner is:\n Elena")
-                                    .font(.system(size: 25, weight: .bold))
-                                    .foregroundColor(.black)
-                                    .multilineTextAlignment(.center)
-                                    .padding(.top, 50.0)
-                                Image("cup")
-                                    .frame(width: 100.0, height: 100.0)
-                                    
-                                Spacer()
-                                Text("Suggested topic:")
-                                    .font(.system(size: 25, weight: .bold))
-                                    .foregroundColor(.black)
-                                    .multilineTextAlignment(.center)
-                                Text("Would you rather?")
-                                    .font(.title) // increased font size of "more"
-                                    .foregroundColor(.clear)
-                                    .background(
-                                        LinearGradient(gradient: Gradient(colors: [.red, .yellow, .green]), startPoint: .leading, endPoint: .trailing)
-                                            .mask(Text("Would you rather?").font(.title))
-                                    )
-                                Spacer()
-                                Button(action: {
-                                    self.isPopupVisible = false
-                                }) {
-                                    Text("OK")
-                                        .font(.system(size: 20, weight: .semibold))
-                                        .foregroundColor(.white)
-                                        .padding(.all, 10)
-                                        .padding(.horizontal, 50.0)
-                                        .background(Color.black)
-                                        .cornerRadius(10)
+                                self.isButtonPressed = true
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                                    self.isPopupVisible = true
                                 }
-                                Spacer()
+                            }) {
+                                Text("Start")
+                                    .font(.system(size: 20, weight: .semibold))
+                                    .foregroundColor(.white)
+                                    .padding(.all, 10)
+                                    .padding(.horizontal, 50.0)
+                                    .background(Color.black)
+                                    .cornerRadius(10)
+                                    .padding(.top, 600.0)
                             }
-                        )
-                        .onTapGesture {
-                            self.isPopupVisible = false
+                            
+                            if isPopupVisible {
+                                // Dimming overlay
+                                Color.black.opacity(0.4)
+                                    .edgesIgnoringSafeArea(.all)
+                                
+                                // Background blur effect
+                                VisualEffectView(effect: UIBlurEffect(style: .systemUltraThinMaterial))
+                                    .edgesIgnoringSafeArea(.all)
+                                
+                                // Popup content
+                                VStack {
+                                    Text("Your coffee partner is:\nElena")
+                                        .font(.system(size: 25, weight: .bold))
+                                        .foregroundColor(.black)
+                                        .multilineTextAlignment(.center)
+                                        .padding(.top, 50.0)
+                                    
+                                    Image("cup")
+                                        .frame(width: 100.0, height: 100.0)
+                                    
+                                    Spacer()
+                                    
+                                    Text("Suggested topic:")
+                                        .font(.system(size: 25, weight: .bold))
+                                        .foregroundColor(.black)
+                                        .multilineTextAlignment(.center)
+                                    
+                                    Text("Would you rather?")
+                                        .font(.title) // increased font size of "more"
+                                        .foregroundColor(.clear)
+                                        .background(
+                                            LinearGradient(gradient: Gradient(colors: [.red, .yellow, .green]), startPoint: .leading, endPoint: .trailing)
+                                                .mask(Text("Would you rather?").font(.title))
+                                        )
+                                    
+                                    Spacer()
+                                    
+                                    Button(action: {
+                                        self.isPopupVisible = false
+                                    }) {
+                                        Text("OK")
+                                            .font(.system(size: 20, weight: .semibold))
+                                            .foregroundColor(.white)
+                                            .padding(.all, 10)
+                                            .padding(.horizontal, 50.0)
+                                            .background(Color.black)
+                                            .cornerRadius(10)
+                                    }
+                                    
+                                    Spacer()
+                                }
+                                .padding()
+                                .background(Color.white)
+                                .cornerRadius(20)
+                                .shadow(radius: 10)
+                                .frame(width: 300, height: 400)
+                            }
                         }
+                    }
+                }
+
+                struct VisualEffectView: UIViewRepresentable {
+                    var effect: UIVisualEffect?
+                    
+                    func makeUIView(context: Context) -> UIVisualEffectView {
+                        UIVisualEffectView()
+                    }
+                    
+                    func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
+                        uiView.effect = effect
+                    }
                 }
             }
-        }
-    }
-}
+
+
+
 
 struct CoffeeBreak_Previews: PreviewProvider {
     static var previews: some View {
@@ -133,3 +158,4 @@ struct CoffeeBreak_Previews: PreviewProvider {
         
     }
 }
+
